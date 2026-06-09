029e7e568325723b44ef4cf5387e0bfdb437b846
Aneesa <Aneesa@DESKTOP-P516BL4>
2026-06-09 13:50:33 +0400

    Port changes

--- Program.cs
+++ Program.cs
@@ -149,4 +149,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 app.Run();
+
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//app.Run($"http://0.0.0.0:{port}");
+//app.Run();
